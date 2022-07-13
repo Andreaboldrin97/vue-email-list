7bf0efd56bdf3547de02748474807a02cb6e1ece
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-07-13 15:33:07 +0200

    created generatorListEmail function

--- js/main.js
+++ js/main.js
@@ -2,17 +2,29 @@ const app = new Vue({
     el : '#root',
     data:
     {
-
+        emailGroup : [],
     },
     methods : {
-        emailgenerator : ()=>{
+        emailGenerator : (array)=>{
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(function (response){
+            .then((response)=>{
                 console.log(response.data.response)
+                array.push(response.data.response) 
+                console.log(array)
             })
+        },
+        generatorListEmail : ()=>{
+            for (let index = 0; index < 10; index++) {
+                this.emailGenerator() 
+            }
         }
+        
+
+
+
     },
     created(){
-        this.emailgenerator();
+        // this.emailGenerator(this.emailGroup)
+        this.generatorListEmail()
     }
 })
